f96975b2b971ebda74a8a426f2a5e5c09af8b1e3
Jonah <jnhev42@gmail.com>
2023-08-27 15:35:48 +0100

    Fix DB schema autoincrement
    Also cleaned up schema.sql to use less verbose syntax

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,49 +1,48 @@
-create table Season (
-    seasonID integer AUTOINCREMENT,
-    startDate date not null,
-    endDate date not null,
-    constraint pkSeason primary key (seasonID),
-    constraint uqSeasonStart unique (startDate),
-    constraint uqSeasonEnd unique (endDate)
+drop table if exists Season;
+create table if not exists Season (
+    seasonID integer PRIMARY KEY AUTOINCREMENT,
+    startDate date NOT NULL,
+    endDate date NOT NULL,
+    unique (startDate),
+    unique (endDate)
 );
 
-create table Deck (
-    deckID integer AUTOINCREMENT,
-    deckName varchar(255) not null,
-    deckLink varchar(511) not null,
-    constraint pkDeck primary key (deckID),
-    constraint uqDeckLink unique (deckLink)
+drop table if exists Season;
+create table if not exists Deck (
+    deckID integer PRIMARY KEY AUTOINCREMENT,
+    deckName varchar(255) NOT NULL,
+    deckLink varchar(511) NOT NULL,
+    unique (deckLink)
 );
 
-create table Player (
-    playerID integer AUTOINCREMENT,
-    playerName varchar(255) not null,
-    constraint pkPlayer primary key (playerID),
-    constraint uqPlayerName unique (playerName)
+drop table if exists Season;
+create table if not exists Player (
+    playerID integer PRIMARY KEY AUTOINCREMENT,
+    playerName varchar(255) NOT NULL,
+    unique (playerName)
 );
 
-create table Tournament (
-    tournamentID integer AUTOINCREMENT,
-    seasonID int not null,
-    tournamentDate date not null,
-    constraint pkTournament primary key (tournamentID),
-    constraint fkTournamentSeason foreign key (seasonID) references Season (seasonID),
-    constraint uqTournamentDate unique (tournamentDate)
+drop table if exists Season;
+create table if not exists Tournament (
+    tournamentID integer PRIMARY KEY AUTOINCREMENT,
+    seasonID int NOT NULL,
+    tournamentDate date NOT NULL,
+    foreign key (seasonID) references Season (seasonID),
+    unique (tournamentDate)
 );
 
-create table TournamentScore (
-    tournamentScoreID integer AUTOINCREMENT,
-    tournamentID int not null,
-    playerID int not null,
-    deckID int not null,
-    OMW numeric(5, 2) not null,
-    wins int not null,
-    losses int not null,
-    draws int not null,
-    points int not null,
-    /* TScore is short for Tournament Score */
-    constraint pkTScore primary key (tournamentScoreID),
-    constraint fkTScoreTournament foreign key (tournamentID) references Tournament (tournamentID),
-    constraint fkTScorePlayer foreign key (playerID) references Player (playerID),
-    constraint fkTScoreDeck foreign key (deckID) references Deck (deckID)
+drop table if exists TournamentScore;
+create table if not exists TournamentScore (
+    tournamentScoreID integer PRIMARY KEY AUTOINCREMENT,
+    tournamentID int NOT NULL,
+    playerID int NOT NULL,
+    deckID int NOT NULL,
+    OMW numeric(5, 2) NOT NULL,
+    wins int NOT NULL,
+    losses int NOT NULL,
+    draws int NOT NULL,
+    points int NOT NULL,
+    foreign key (tournamentID) references Tournament (tournamentID),
+    foreign key (playerID) references Player (playerID),
+    foreign key (deckID) references Deck (deckID)
 );
