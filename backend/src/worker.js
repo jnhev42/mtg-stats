dfa51d542bb51bcc80655ae898d11a6d87a2b015
matt <mmjclay04@gmail.com>
2023-09-10 19:47:07 +0100

    back-end router+SQL_players statement

--- backend/src/worker.js
+++ backend/src/worker.js
@@ -8,13 +8,36 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import {
+    error,      // creates error responses
+    json,       // creates JSON responses
+    Router,     // the ~440 byte router itself
+    withParams, // middleware: puts params directly on the Request
+  } from 'itty-router'
+  
+const router = Router() // no "new", as this is not a real class
+
+//example register a route on the "GET" method
+router.get('/todos/:id', (request) => 
+{
+    const { params } = request
+
+    return `You've asked for todo #${params.id}.`
+})
+
+//register a route to show all players
+router.get('/All_Players', (request,env,) => 
+{
+  return env.DB.prepare ('SELECT * FROM Player;').raw()
+})
+
+  // we can chain definitions to reduce boilerplate
+  .get('*', () => error(404))
+
+ 
 export default {
-	async fetch(request, env, ctx) {
-		const corsHeaders = {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-			"Access-Control-Max-Age": "86400",
-		  };
-		return new Response('Hello Me!', { headers: corsHeaders });
-	},
-};
+  fetch: (req, env, ctx) => router
+                              .handle(req, env, ctx)
+                              .then(json)
+                              .catch(error)
+}
