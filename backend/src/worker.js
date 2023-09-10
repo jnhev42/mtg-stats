91e4b2573d077a6e84426328d0eaf5062b055186
Jonah <jnhev42@gmail.com>
2023-09-10 22:37:18 +0100

    Fix CORS issues

--- backend/src/worker.js
+++ backend/src/worker.js
@@ -1,43 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import {
-    error,      // creates error responses
-    json,       // creates JSON responses
-    Router,     // the ~440 byte router itself
-    withParams, // middleware: puts params directly on the Request
+    error,
+    json,
+    Router,
+    createCors,
   } from 'itty-router'
   
-const router = Router() // no "new", as this is not a real class
+const router = Router()
 
-//example register a route on the "GET" method
-router.get('/todos/:id', (request) => 
-{
-    const { params } = request
-
-    return `You've asked for todo #${params.id}.`
+const { preflight, corsify } = createCors({
+  origins: ['*'],
+  methods: ['GET', 'POST'],
 })
+router.all("*", preflight)
 
 //register a route to show all players
-router.get('/All_Players', (request,env,) => 
-{
+router.get('/All_Players', (_, env) => {
   return env.DB.prepare ('SELECT * FROM Player;').raw()
 })
 
-  // we can chain definitions to reduce boilerplate
-  .get('*', () => error(404))
+router.get('*', () => error(404))
 
  
 export default {
   fetch: (req, env, ctx) => router
-                              .handle(req, env, ctx)
-                              .then(json)
-                              .catch(error)
+    .handle(req, env, ctx)
+    .then(json)
+    .catch(error)
+    .then(corsify)
 }
